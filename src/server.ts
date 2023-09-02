abe984a2f60ec2b44c41ae8702f92e95e1a749d7
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-09-02 10:26:49 -0300

    feat: install husky and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 33333
-}).then(() => {
-  console.log('listening server run on port 3333')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: 33333,
+  })
+  .then(() => {
+    console.log('listening server run on port 3333')
+  })
