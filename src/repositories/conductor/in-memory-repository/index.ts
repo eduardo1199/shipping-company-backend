ce98ae3bab3527c5d0385ef56765bc1c84d9ba84
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-01-10 16:42:26 -0300

    feat: create repository in memory and use case create conductor

---
+++ src/repositories/conductor/in-memory-repository/index.ts
@@ -0,0 +1,58 @@
+import { Conductor, Prisma } from '@prisma/client'
+import { IConductorRepository } from '../conductor-repository'
+import { randomUUID } from 'crypto'
+
+export class InMemoryConductorRepository implements IConductorRepository {
+  public conductors: Conductor[] = []
+
+  async create(data: Prisma.ConductorCreateInput) {
+    const id = randomUUID()
+
+    const conductorData: Conductor = {
+      ...data,
+      register: new Date(),
+      created_at: new Date(),
+      id,
+    }
+
+    this.conductors.push(conductorData)
+
+    return conductorData
+  }
+
+  async findByCPF(cpf: string) {
+    const findSameConductorWithCPF = this.conductors.find(
+      (conductor) => conductor.cpf === cpf,
+    )
+
+    if (!findSameConductorWithCPF) {
+      return null
+    }
+
+    return findSameConductorWithCPF
+  }
+
+  async findByEmail(email: string) {
+    const findSameConductorWithEmail = this.conductors.find(
+      (conductor) => conductor.email === email,
+    )
+
+    if (!findSameConductorWithEmail) {
+      return null
+    }
+
+    return findSameConductorWithEmail
+  }
+
+  async findByCatHab(cat_hab: number) {
+    const findSameConductorWithCatHab = this.conductors.find(
+      (conductor) => conductor.cat_hab === cat_hab,
+    )
+
+    if (!findSameConductorWithCatHab) {
+      return null
+    }
+
+    return findSameConductorWithCatHab
+  }
+}
